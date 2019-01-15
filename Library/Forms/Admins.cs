c7e196e363c96c22ca33281b35bf4d8f03a25f77
aqashovas <42311189+aqashovas@users.noreply.github.com>
2019-01-15 21:33:09 +0400

    Update Admins.cs

--- Library/Forms/Admins.cs
+++ Library/Forms/Admins.cs
@@ -15,7 +15,7 @@ namespace Library.Forms
 {
     public partial class Admins : MetroForm
     {
-        private readonly LibraryEntities db = new LibraryEntities();
+        private readonly LibraryEntities1 db = new LibraryEntities1();
         private int SelectedId;
         public Admins()
         {
@@ -58,6 +58,125 @@ namespace Library.Forms
                     alllabel.Visible = false;
                 }
 
+
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (db.Admins.Any(a => a.ALogName == txtLogname.Text))
+            {
+                MetroFramework.MetroMessageBox.Show(this, "This admin has already sign uped!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                Admin admin = new Admin
+                {
+                    ALogName = txtLogname.Text,
+                    AName = txtName.Text.Split(' ')[1],
+                    ASurname = txtName.Text.Split(' ')[0],
+                    
+                };
+                if (comp.Visible == true)
+                {
+                    admin.Password = txtPass.Text;
+                }
+                else
+                {
+                    MetroFramework.MetroMessageBox.Show(this, "Please,choose correct password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                FillData();
+                Reset();
+            }
+        }
+
+        private void Reset()
+        {
+            txtLogname.ResetText();
+            txtName.ResetText();
+            txtPass.ResetText();
+            
+        }
+        private void FillData()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var item in db.Admins.ToList())
+            {
+                dataGridView1.Rows.Add(item.Id, item.ASurname + " " + item.AName, item.ALogName, item.Password);
+            }
+        }
+        private void txtSearh_Click(object sender, EventArgs e)
+        {
+            txtSearh.ResetText();
+            FillData();
+            
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            SelectedId =Convert.ToInt32( dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+            txtLogname.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtName.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtPass.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            btnAdd.Visible = false;
+            btnDel.Visible = true;
+            btnUpdate.Visible = true;
+
+
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+
+            DialogResult r = MetroFramework.MetroMessageBox.Show(this, "Are you sure?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (r == DialogResult.Yes)
+            {
+                db.Admins.Remove(db.Admins.Find(SelectedId));
+                db.SaveChanges();
+
+            }
+            Reset();
+            btnAdd.Visible = true;
+            btnDel.Visible = false;
+            btnUpdate.Visible = false;
+            FillData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            foreach (var item in db.Admins.Where(a=>a.Id==SelectedId).ToList())
+            {
+                item.AName = txtName.Text.Split(' ')[1];
+                item.ASurname = txtName.Text.Split(' ')[0];
+                item.Password = txtPass.Text;
+
+            }
+            db.SaveChanges();
+            btnAdd.Visible = true;
+            btnDel.Visible = false;
+            btnUpdate.Visible = false;
+            FillData();
+            Reset();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            if (db.Admins.Any(u => u.AName.Contains(txtSearh.Text) || u.ASurname.Contains(txtSearh.Text) || u.ALogName.Contains(txtSearh.Text)))
+            {
+                dataGridView1.Rows.Clear();
+                foreach (var item in db.Admins.Where(u => u.AName.Contains(txtSearh.Text) || u.ASurname.Contains(txtSearh.Text) || u.ALogName.Contains(txtSearh.Text)).ToList())
+                {
+                    dataGridView1.Rows.Add(item.Id, item.ASurname + " " + item.AName, item.ALogName, item.Password);
+
+                }
+            }
+            else
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Not Found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
         }
