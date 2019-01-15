3aaa66ef242cd2b5108f43f5ff7d16e8b7f41a9f
aqashovas <42311189+aqashovas@users.noreply.github.com>
2019-01-15 21:37:28 +0400

    Update FUsers.cs

--- Library/Forms/FUsers.cs
+++ Library/Forms/FUsers.cs
@@ -15,7 +15,7 @@ namespace Library.Forms
 {
     public partial class FUsers : MetroForm
     {
-        private readonly LibraryEntities1 db = new LibraryEntities1();
+        private readonly LibraryEntities db = new LibraryEntities();
         private int SelectedId;
         public FUsers()
         {
